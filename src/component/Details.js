962863f445096c69e8dfe40fa13a68c32fc8d633
badari07 <badari0793@gmail.com>
2020-01-08 10:57:44 +0530

    adding router and details page

--- src/component/Details.js
+++ src/component/Details.js
@@ -1,7 +1,51 @@
 import React from "react";
+import { Client } from "@petfinder/petfinder-js";
+import Carousel from "./Carousel";
 
-const Details = () => {
-  return <h1>hi!</h1>;
-};
+const client = new Client({
+  apiKey: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
+class Details extends React.Component {
+  state = { loading: true };
+
+  componentDidMount() {
+    client.animal
+      .show(+this.props.id)
+      .then(resp => {
+        let pet = resp.data.animal;
+        this.setState({
+          name: pet.name,
+          animal: pet.type,
+          location: `${pet.contact.address.city}, ${pet.contact.address.state}`,
+          description: pet.description,
+          media: pet.photos,
+          breed: pet.breeds.primary,
+          loading: false
+        });
+      })
+      .catch(err => this.setState({ error: err }));
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>loading … </h1>;
+    }
+
+    const { animal, breed, location, description, media, name } = this.state;
+
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
